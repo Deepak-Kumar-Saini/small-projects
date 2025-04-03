d4dc3bd74b9124c460311d755d31ed04554753b6
deepak-kumar-saini2 <876467deepak@gmail.com>
2025-04-03 10:20:43 +0530

    add functionality to random color btn using js

--- Background color changer/script.js
+++ Background color changer/script.js
@@ -1,9 +1,14 @@
 const btns = document.querySelectorAll(".btn");
 const body = document.querySelector("body");
+const randomBtn = document.querySelector(".random-btn");
 
 btns.forEach((btn)=>{
     btn.style.background = btn.id;
     btn.addEventListener("click",()=>{
         body.style.backgroundColor = btn.id;
     })
+})
+
+randomBtn.addEventListener("click",()=>{
+        body.style.backgroundColor = btns[Math.floor(Math.random()*6)].id;
 })
